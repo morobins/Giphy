904aa2446fd1e36e421606314668f34416e0600d
Monique Robinson <monique.robinson2@gmail.com>
2018-03-06 13:30:05 -0500

    Got form to work and add buttons but it won't render the gifs once clicked

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -31,7 +31,29 @@ function renderButtons() {
 
 };
 
+$("#add-artist").on("click", function (event) {
+  // event.preventDefault() prevents submit button from trying to send a form.
+  // Using a submit button instead of a regular button allows the user to hit
+  // "Enter" instead of clicking the button if desired
+  event.preventDefault();
+
+  // Write code to grab the text the user types into the input field
+  
+ 
+  if ($("#artist-input").val() !== '') {
+
+  var newArtist = $("#artist-input").val();
+  // Write code to add the new movie into the movies array
+  topics.push(newArtist);
+
   renderButtons();
+
+  };
+
+});
+
+renderButtons();
+  
 
 
 
